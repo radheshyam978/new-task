2c8ea7dc2241fde84de7e4a7eb3444952dafdcfa
radheshyam978 <vaishnavradhey978@gmail.com>
2025-09-23 16:05:17 +0530

    done

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const fetchItems = async (skip = 0, limit = 20) => {
-  const res = await axios.get(`http://localhost:4000/api/items?skip=${skip}&limit=${limit}`);
+  const res = await axios.get(`https://new-task-3.onrender.com/`);
   return res.data; // { items: [...], totalCap: 100 }
 };
